b9003a023882010d57a02e5bc2328844f933fee5
Johnathan Guzman <45727071+johnathanthedev@users.noreply.github.com>
2023-01-27 02:43:16 -0600

    Setup db connection and containerized project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+
+mod config;
+mod controllers;
+
+use config::db::set_up;
+use controllers::root_controller::index;
+
+#[launch]
+async fn rocket() -> _ {
+    let db = match set_up().await {
+        Ok(db) => db,
+        Err(err) => panic!("{}", err),
+    };
+
+    rocket::build().manage(db).mount("/", routes![index])
 }
