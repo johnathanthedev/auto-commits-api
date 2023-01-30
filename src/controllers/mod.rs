a2dd63c30450d87eadf34393f6561e15894a1b1f
Johnathan Guzman <45727071+johnathanthedev@users.noreply.github.com>
2023-01-30 03:43:07 -0600

    Working on db interaction

--- src/controllers/mod.rs
+++ src/controllers/mod.rs
@@ -1,1 +1,2 @@
+pub mod accounts_controller;
 pub mod root_controller;
--- src/controllers/root_controller.rs
+++ src/controllers/root_controller.rs
@@ -2,13 +2,13 @@ use rocket::serde::{json::Json, Deserialize, Serialize};
 
 #[derive(Debug, Deserialize, Serialize)]
 #[serde(crate = "rocket::serde")]
-pub struct TestMessage {
+pub struct Message {
     pub message: String,
 }
 
 #[get("/")]
-pub fn index() -> Json<TestMessage> {
-    return Json(TestMessage {
+pub fn index() -> Json<Message> {
+    return Json(Message {
         message: String::from("Welcome to the Auto Commits API"),
     });
 }
